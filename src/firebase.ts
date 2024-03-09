f46c1e12f9244463e6a4477882e78787f9e38ca1
Lucho Web <luchoweb.dev@gmail.com>
2024-03-09 12:54:18 -0500

    Firebase setup filed added

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const {
+  VITE_FIREBASE_API_KEY: apiKey,
+  VITE_FIREBASE_AUTH_DOMAIN: authDomain,
+  VITE_FIREBASE_PROJECT_ID: projectId,
+  VITE_FIREBASE_BUCKET: storageBucket,
+  VITE_FIREBASE_SENDER_ID: messagingSenderId,
+  VITE_FIREBASE_APP_ID: appId,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
