f77ba2422f01ea6e2b4755abef1b8278cd894a93
Lucho Web <luchoweb.dev@gmail.com>
2024-03-12 10:53:28 -0500

    movie card done

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const MovieCard = ({ movie }: Props) => {
-  const { BASE_URL_MOVIE_IMAGES: bgBaseUrl } = APP;
+  const { BASE_URL_MOVIE_POSTER: bgBaseUrl } = APP;
 
   return (
     <Link
@@ -18,13 +18,10 @@ const MovieCard = ({ movie }: Props) => {
         .toLowerCase()}`}
       className="movie-card bg-dark text-decoration-none text-center p-5"
       style={{
-        backgroundImage: `url(${bgBaseUrl}${movie.backdrop_path})`,
+        backgroundImage: `url(${bgBaseUrl}${movie.poster_path})`,
         backgroundSize: "cover",
       }}
-    >
-      <span className="movie-card-overlay"></span>
-      <span className="movie-card-name text-light">{movie.original_title}</span>
-    </Link>
+    ></Link>
   );
 };
 
