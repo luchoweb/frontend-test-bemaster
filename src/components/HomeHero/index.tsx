4f15bab861b269b87443eea7d877a9ea3bae21cf
Lucho Web <luchoweb.dev@gmail.com>
2024-03-12 15:00:29 -0500

    prettier home hero

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -7,10 +7,12 @@ const HomeHero = () => {
 
       <div className="container text-end">
         <h1 className="m-0 text-light">Welcome, Padawan!</h1>
-        <h5 className="fw-normal m-0 text-light">Enjoy the best movies on JEDI TV</h5>
+        <h5 className="fw-normal m-0 text-light">
+          Enjoy the best movies on JEDI TV
+        </h5>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default HomeHero;
